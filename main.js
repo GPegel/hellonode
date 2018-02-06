f6dd76a576d50f09ef7c023a086222b6d715442f
Gerhard Pegel <gpegel@gmail.com>
2018-02-06 14:48:50 +0100

    another test another port

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello gpegel.nl, this is anoter test\n");
+  response.end("Hello gpegel.nl, this is anoter test on another port\n");
 });
 
 // listen on localhost:8000
