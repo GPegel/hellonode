efbada29d457f7171f345caaa49cea0c4b403efa
Gerhard Pegel <gpegel@gmail.com>
2018-02-06 14:39:24 +0100

    another test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello gpegel.nl\n");
+  response.end("Hello gpegel.nl, this is a test\n");
 });
 
 // listen on localhost:8000
